3c54937dee2ebe3001f0662af08d8e9510779a83
Neel Crasta <neeldc2@gmail.com>
2026-05-16 10:23:30 +0530

    second commit

---
+++ src/main/java/com/example/spring_ai/config/RagConfiguration.java
@@ -0,0 +1,56 @@
+package com.example.spring_ai.config;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.ai.document.Document;
+import org.springframework.ai.reader.TextReader;
+import org.springframework.ai.transformer.splitter.TextSplitter;
+import org.springframework.ai.transformer.splitter.TokenTextSplitter;
+import org.springframework.ai.vectorstore.SimpleVectorStore;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.ai.embedding.EmbeddingModel;
+
+import java.io.File;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+@Slf4j
+@Configuration
+public class RagConfiguration {
+
+    @Value("classpath:docs/olympic-faq.txt")
+    private Resource faqResource;
+
+    @Value("vector-store.json")
+    private String vectorStoreName;
+
+    @Bean
+    SimpleVectorStore simpleVectorStore(final EmbeddingModel embeddingModel) {
+        SimpleVectorStore simpleVectorStore = SimpleVectorStore.builder(embeddingModel).build();
+        File vectorStoreFile = getVectorStoreFile();
+        if (vectorStoreFile.exists()) {
+            log.info("Vector Store File Exists,");
+            simpleVectorStore.load(vectorStoreFile);
+        } else {
+            log.info("Vector Store File Does Not Exist, loading documents");
+            TextReader textReader = new TextReader(faqResource);
+            textReader.getCustomMetadata().put("filename", "olympic-faq.txt");
+            List<Document> documents = textReader.get();
+            TextSplitter textSplitter = new TokenTextSplitter();
+            List<Document> splitDocuments = textSplitter.apply(documents);
+            simpleVectorStore.add(splitDocuments);
+            simpleVectorStore.save(vectorStoreFile);
+        }
+        return simpleVectorStore;
+    }
+
+    private File getVectorStoreFile() {
+        Path path = Paths.get("src", "main", "resources", "data");
+        String absolutePath = path.toFile().getAbsolutePath() + "/" + vectorStoreName;
+        return new File(absolutePath);
+    }
+
+}
